4ebe6ac0ad8cfadf564ee3eb7dc75dd3b6405c40
Saken112 <saken.baubekuly@mail.ru>
2025-04-08 22:38:44 +0500

    new commit

--- studentmanagement/education/views.py
+++ studentmanagement/education/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render, redirect
-from .models import Student
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from .models import Student
 from .forms import CustomUserCreationForm
-
+from django.db.models import Q
+from django.views.generic.edit import CreateView
 
 
 @login_required
 def student_list(request):
-    students = Student.objects.all()
+    query = request.GET.get('q', '').strip()
+
+    if query:
+        students = Student.objects.filter(
+            Q(first_name__icontains=query) |
+            Q(last_name__icontains=query) |
+            Q(email__icontains=query)
+        )
+    else:
+        students = Student.objects.all()
+
     return render(request, 'education/student_list.html', {'students': students})
 
 def register(request):
@@ -22,7 +30,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('student_list')  # перенаправление на список студентов после регистрации
+            return redirect('student_list')
     else:
         form = UserCreationForm()
     return render(request, 'education/register.html', {'form': form})
@@ -36,15 +44,12 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('student_list')  # перенаправление после логина
+                return redirect('student_list')
             else:
-                form.add_error(None, 'Неверные данные')
+                form.add_error(None, 'Неверные данные для входа')
     else:
         form = AuthenticationForm()
     return render(request, 'education/login.html', {'form': form})
-
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 
 @login_required
 def profile(request):
@@ -54,4 +59,3 @@ class RegisterView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'education/register.html'
     success_url = reverse_lazy('login')
-
