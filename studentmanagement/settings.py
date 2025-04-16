e0af2ed064349ce54bf6ca9db7c6e686d6688d74
Saken112 <saken.baubekuly@mail.ru>
2025-04-16 16:04:07 +0500

    4 week

--- studentmanagement/settings.py
+++ studentmanagement/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'accounts',
     'education',
     'main',
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
